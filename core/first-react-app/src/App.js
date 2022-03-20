ebb471d659c0e3607723544d64b405f8e277d46a
Ramsy Anton <anton.ra@hotmail.com>
2022-03-20 09:49:03 -0700

    First React App

---
+++ core/first-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      Things I need to do:
+      <ul>
+        <li>Learn React</li>
+        <li>Clim Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
